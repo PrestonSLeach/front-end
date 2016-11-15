1f5af89727ecf054142572a100cc110bedfdabd3
Preston Leach <prestonsleach@gmail.com>
2016-11-15 12:05:28 -0600

    added the change form and buttons for editUser

--- src/app/user/editUser.service.js
+++ src/app/user/editUser.service.js
@@ -1,17 +1,60 @@
 export class EditUserService {
 
-  constructor ($log) {
+  constructor ($log, $http) {
     'ngInject'
-    // options listed in edit profile
-    this.editUsername
-    this.editPassword
-    this.editFirstName
-    this.editLastName
-    this.editPhoneNumber
-    this.deleteAccount
-    this.editEmail
+    this.$http = $http
+    // fields listed in edit profile
+    this.username
+    this.password
+    this.firstName
+    this.lastName
+    this.phoneNumber
+    this.email
     // optional?
     // editPrivacy
     $log.debug('EditUserService instantiated')
   }
+
+  editUser () {
+    this.$http({
+      method: 'PATCH',
+      url: 'http://localhost:8080/users/@myUsername',
+      headers: {
+        'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000/'
+      }
+    }).then(function successCallback (response) {
+      console.log(response.data)
+    }, function errorCallback (response) {
+      console.log(response)
+    })
+  }
+
+  deleteAccount () {
+    this.$http({
+      method: 'DELETE',
+      url: 'http://localhost:8080/users/@{username}',
+      headers: {
+        'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000/'
+      }
+    }).then(function successCallback (response) {
+      console.log(response.data)
+    }, function errorCallback (response) {
+      console.log(response)
+    })
+  }
+
+  user = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: ''
+
+  };
+
 }
