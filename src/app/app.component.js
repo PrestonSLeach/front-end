b18c5d4b8d07e2e981facac7c84e3b3f42d580a2
Adrian <adrian.devezin@gmail.com>
2016-11-16 08:49:49 -0600

    added toolbar

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -2,83 +2,131 @@ import templateUrl from './app.component.html'
 
 /* @ngInject */
 class AppController {
-  constructor ($log, $http, $q, $timeout) {
-    this.$log = $log
-    this.$log.debug('AppController instantiated')
-    var originatorEv;
-    this.$http = $http
-    this.$q = $q
-    var self = this;
-    this.loadAll()
-    self.simulateQuery = false;
-    self.isDisabled    = false;
-    self.querySearch   = this.querySearch();
-   self.selectedItemChange = this.selectedItemChange();
-   self.searchTextChange   = this.searchTextChange();
+    constructor($log, $http, $q, $timeout) {
+        var self = this;
+        this.$http = $http
+        self.simulateQuery = false;
+        self.isDisabled = false;
 
-    this.openMenu = function($mdOpenMenu, ev) {
-      console.log('open clicked');
-      originatorEv = ev;
-      $mdOpenMenu(ev);
-    };
-    this.openProfile = function() {
-    //redirect to users profile
+        // list of `state` value/display objects
+        this.loadAll();
+        console.log('states is below')
+        console.log(self.states)
+        self.querySearch = querySearch;
+        self.selectedItemChange = selectedItemChange;
+        self.searchTextChange = searchTextChange;
+
+        self.newState = newState;
+        this.wtf = 'wtf'
+
+        function newState(state) {
+            alert("Sorry! You'll need to create a Constitution for " + state + " first!");
+        }
+
+        function querySearch(query) {
+            var results = query ? self.states.filter(createFilterFor(query)) : self.states,
+                deferred;
+            if (self.simulateQuery) {
+                deferred = $q.defer();
+                $timeout(function () {
+                    deferred.resolve(results);
+                }, Math.random() * 1000, false);
+                return deferred.promise;
+            } else {
+                return results;
+            }
+        }
+
+        function searchTextChange(text) {
+            $log.info('Text changed to ' + text);
+        }
+
+        function selectedItemChange(item) {
+            $log.info('Item changed to ' + JSON.stringify(item));
+        }
+
+        /**
+         * Build `states` list of key/value pairs
+         */
+
+
+        /**
+         * Create filter function for a query string
+         */
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+
+            return function filterFn(state) {
+                return (state.value.indexOf(lowercaseQuery) === 0);
+            };
+
+        }
+
+        function findLabel(state) {
+            return state.label === 'idiot';
+        }
+
+
     }
-
-
-
-  }
-
-  loadAll() {
-    console.log('loadall')
-    var allStates= []
-          let appController = this
-          this.$http({
+    loadAll () {
+        var allStates = []
+        var control = this
+        console.log('control below')
+        console.log(control)
+        this.$http({
             method: 'GET',
             url: 'http://localhost:8080/tags',
             headers: {
-              'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': 'http://localhost:3000/'
+                'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'http://localhost:3000/'
             }
-          }).then(function successCallback (response) {
-            appController.states = response.data
-            console.log('done loading')
-          }, function errorCallback (response) {
+        }).then(function successCallback(response) {
+            allStates = response.data
+            allStates = allStates.map(function (state) {
+                return {
+                    value: state.label.substring(1),
+                    display: state.label
+                };
+            });
+            console.log(allStates);
+            control.states = allStates
+            loadUsersIntoAll()
+        }, function errorCallback(response) {
             console.log(response)
-          })
-          return allStates;
-      }
-
-    querySearch(query) {
-      console.log('insert query')
-      if (!query) return;
-            var results = query ? this.states.filter( createFilterFor(query) ) : this.states,
-                deferred;
-
-            return results;
-
-        }
-
-        searchTextChange(text) {
-          if (text === null || text === "" || text === undefined)
-        return [];
-
-          console.log('textchange')
-            this.$log.info('Text changed to ' + text);
-        }
-
-        selectedItemChange(item) {
-          if (item === null || item === "" || item === undefined)
-        return [];
-          console.log('itemquery')
-            this.$log.info('Item changed to ' + JSON.stringify(item));
-        }
-
+        })
+    }
+    loadUsersIntoAll () {
+        var allStates = []
+        var control = this
+        console.log('control below')
+        console.log(control)
+        this.$http({
+            method: 'GET',
+            url: 'http://localhost:8080/users',
+            headers: {
+                'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'http://localhost:3000/'
+            }
+        }).then(function successCallback(response) {
+            allStates = response.data
+            allStates = allStates.map(function (state) {
+                return {
+                    value: state.label.substring(1),
+                    display: state.label
+                };
+            });
+            console.log(allStates);
+            control.states.concat(allStates)
+        }, function errorCallback(response) {
+            console.log(response)
+        })
+    }
 }
 
 export const app = {
-  templateUrl,
-  controller: AppController,
-  controllerAs: '$app'
+    templateUrl,
+    controller: AppController,
+    controllerAs: '$app'
 }
