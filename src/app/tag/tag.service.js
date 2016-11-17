c9cdf6b8a1f1e22164f62a076fd9f7def9ae33c7
Brad Lindgren <bplindgren91@gmail.com>
2016-11-17 11:11:39 -0600

    tags regex

--- src/app/tag/tag.service.js
+++ src/app/tag/tag.service.js
@@ -26,7 +26,9 @@ export class TagService {
         .map(tweet => {
           tweet.content = tagService.$sce.trustAsHtml(tweet.content
             .split(' ')
-            .map(word => (word.substring(0, 1) === '#') ? "<ui-sref='tag({tag:" + word.substring(1, / #\\w+/) + "})'><a href='tag/" + word.substring(1) + "' style='text-decoration: none'>" + word + "</a>" : word)
+            .map(word => 
+              (word.substring(0, 1) === '#') ? "<ui-sref='tag({tag:" + word.substring(1).toLowerCase() + "})'><a href='tag/" + word.substring(1).toLowerCase() + "' style='text-decoration: none'>" + word + "</a>" : 
+              (word.substring(0, 1) === '@') ? "<ui-sref='user({user:" + word.substring(1).toLowerCase() + "})'><a href='user/" + word.substring(1).toLowerCase() + "' style='text-decoration: none'>" + word + "</a>" : word)
             .join(' '))
           return tweet
         })
