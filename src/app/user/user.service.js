d29037a2ee13172746157b9dcf9419b2f4090f6f
Adrian <adrian.devezin@gmail.com>
2016-11-18 10:10:19 -0600

    added more to tweet, added methods for sidenav, made followers/following a list

--- src/app/user/user.service.js
+++ src/app/user/user.service.js
@@ -7,7 +7,8 @@ export class UserService {
     this.$config = $config
     this.$http = $http
     this.$cookies = $cookies
-    
+    this.getPopular();
+    this.getTrending();
     $log.debug('UserService instantiated!')
   }
 
@@ -19,7 +20,22 @@ export class UserService {
       return false
   }
 
+  people = [
+           { name: 'Janet Perkins', img: 'img/100-0.jpeg', newMessage: true },
+           { name: 'Mary Johnson', img: 'img/100-1.jpeg', newMessage: false },
+           { name: 'Peter Carlsson', img: 'img/100-2.jpeg', newMessage: false }
+       ];
 
+       goToPerson = function(person, event) {
+           $mdDialog.show(
+               $mdDialog.alert()
+                   .title('Navigating')
+                   .textContent('Inspect ' + person)
+                   .ariaLabel('Person inspect demo')
+                   .ok('Neat!')
+                   .targetEvent(event)
+           );
+       };
 
   followUser (username) {
     let cookies = this.$cookies
@@ -37,6 +53,44 @@ export class UserService {
       }
     }).then(function successCallback (response) {
       console.log(response.data)
+    }, function errorCallback (response) {
+      console.log(response)
+    })
+  }
+
+  getPopular () {
+    let cookies = this.$cookies
+    let userService = this
+    this.$http({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets/popular',
+      headers: {
+        'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000/'
+      }
+    }).then(function successCallback (response) {
+      response.data.slice(0,3)
+      userService.popularTweets = response.data
+    }, function errorCallback (response) {
+      console.log(response)
+    })
+  }
+
+  getTrending () {
+    let cookies = this.$cookies
+    let userService = this
+    this.$http({
+      method: 'GET',
+      url: 'http://localhost:8080/tags/trending',
+      headers: {
+        'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000/'
+      }
+    }).then(function successCallback (response) {
+      response.data.slice(0,3)
+      userService.trendingTags = response.data
     }, function errorCallback (response) {
       console.log(response)
     })
